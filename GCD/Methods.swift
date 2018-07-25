a44f45780725993fbede3c45ebe69fbc790c2812
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-07-25 14:30:46 +0300

    Async vs Sync Concurrent

--- GCD/Methods.swift
+++ GCD/Methods.swift
@@ -2,6 +2,8 @@ import Foundation
 
 // Async vs Sync
 
+// Serial Queue
+
 class AsyncVsSyncTest1 {
   private let serialQueue = DispatchQueue(label: "serialTest")
   
@@ -22,4 +24,25 @@ class AsyncVsSyncTest1 {
   }
 }
 
+// Concurrent Queue
 
+class AsyncVsSyncTest2 {
+  private let concurrentQueue = DispatchQueue.global()
+  
+  func testConcurrent() {
+    concurrentQueue.async {
+      print("test1")
+    }
+    concurrentQueue.async {
+      sleep(1)
+      print("test2")
+    }
+    concurrentQueue.sync {
+      print("test3")
+    }
+    concurrentQueue.sync {
+      print("test4")
+    }
+  }
+}
+AsyncVsSyncTest2().testConcurrent()
